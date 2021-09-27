1f569004c11dcb003095768076a9c425f56f7cac
Apollo3zehn <vwilms@outlook.com>
2021-09-27 12:12:25 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Intel.ISAL.PInvoke
+
+[![AppVeyor](https://ci.appveyor.com/api/projects/status/github/apollo3zehn/Intel.ISAL.PInvoke?svg=true&branch=master)](https://ci.appveyor.com/project/Apollo3zehn/intel-isal-pinvoke)
+[![NuGet](https://img.shields.io/nuget/vpre/Intel.ISAL.PInvoke.svg?label=Nuget)](https://www.nuget.org/packages/Intel.ISAL.PInvoke)
+
+This lib simply provides a collection of P/Invoke signatures for Linux and Windows to enable working with [Intel Intelligent Storage Acceleration Library](https://github.com/intel/isa-l) using .NET.
---
+++ tests/Intel.ISA-L.PInvoke.Tests/PInvokeTests.cs
@@ -0,0 +1,30 @@
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Xunit;
+using RuntimeEnvironment = Microsoft.DotNet.PlatformAbstractions.RuntimeEnvironment;
+
+namespace Intel.ISAL.PInvoke.Tests
+{
+    public class PInvokeTests
+    {
+        [Fact]
+        public void CanAccessNativeLib()
+        {
+            // Arrange
+            Directory.EnumerateFiles("./runtimes/", "*blosc2.*", SearchOption.AllDirectories).ToList().ForEach(filePath =>
+            {
+                if (filePath.Contains(RuntimeEnvironment.RuntimeArchitecture))
+                {
+                    File.Copy(filePath, Path.GetFileName(filePath), true);
+                }
+            });
+
+            // Act
+            var compressors = Marshal.PtrToStringAnsi(Blosc.blosc_list_compressors());
+
+            // Assert
+            Assert.True(compressors == "blosclz,lz4,lz4hc,lizard,zlib,zstd");
+        }
+    }
+}
